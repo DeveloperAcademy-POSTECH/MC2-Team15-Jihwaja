2f5421300d2479fdc2ce0a7326aa1344f4f85bee
geee3 <c1as2icblu3@gmail.com>
2023-05-02 16:10:55 +0900

    🔧 feat: 세팅 Xcode 프로젝트

---
+++ Jihwaja/JihwajaApp.swift
@@ -0,0 +1,17 @@
+//
+//  JihwajaApp.swift
+//  Jihwaja
+//
+//  Created by geee3 on 2023/05/02.
+//
+
+import SwiftUI
+
+@main
+struct JihwajaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
