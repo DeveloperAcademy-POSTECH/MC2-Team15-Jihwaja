64efc66f06592f0b392a2957a83b645b6eabe601
MK827 <bluebonnet712@gmail.com>
2023-05-08 12:18:56 +0900

    💄 fix: 수정 Q2view 슬라이더 색상 수정 (#13)

--- Jihwaja/Views/Questions/QuestionView02.swift
+++ Jihwaja/Views/Questions/QuestionView02.swift
@@ -8,7 +8,8 @@
 import SwiftUI
 
 struct QuestionView02: View {
-    @State private var yes = 10.0
+    @State private var yes = 1.0
+    @State private var isActiveQ2 = false
     
     var body: some View {
         NavigationView{
@@ -21,13 +22,17 @@ struct QuestionView02: View {
                         .foregroundColor(Color("green"))
                 }.frame(width: getWidth(), height: getHeight() * 0.4)
                 Slider(value: $yes,
-                       in: 1...100,
-                       step: 1)  
+                       in: 1...101,
+                       step: 1
+                )
+                .onChange(of: yes) { _ in
+                    isActiveQ2 = true
+                }
                 .accentColor(Color("green"))
-                Text("\(Int(yes)) %")
-   
                 
-                StoreButtonView(isActive: true)
+                Text("\(Int(yes)-1) %")
+
+                StoreButtonView(isActive: isActiveQ2)
             }
             .padding(.horizontal)
         }
