61af29d82c6219fa1202c17d168ff9c5b030619a
MK827 <bluebonnet712@gmail.com>
2023-05-09 10:57:33 +0900

    :sparkles: feat: 추가 묘비 답변 기능 (#14)

--- Jihwaja/Views/Questions/QuestionView04.swift
+++ Jihwaja/Views/Questions/QuestionView04.swift
@@ -9,35 +9,37 @@ import SwiftUI
 
 struct QuestionView04: View {
     
-    @State private var isActiveQ4 = false
     @State private var text : String = ""
 
     var body: some View {
         NavigationView{
             VStack{
                 QuestionView(question: "💀 곽애숙씨가 훗날 기억되길 원하는 문구가 있다면 적어주세요!")
-               
                Image("gravestone")
-                    .overlay{
-                        ZStack{
-                            Text("이곳에 적어주세요")
-                                .opacity(text.isEmpty ? 1 : 0)
-                            TextEditor(
-                                text: $text)
-                            //.opacity(0.5)
-                            .padding()
-                            .border(.red)
-                            .scrollContentBackground(.hidden)
-                        }
+                    .overlay {
+                        Text("이곳에 적어주세요")
+                            .opacity(text.isEmpty ? 1 : 0)
+                                //geometryReader에 대한 공부 필요...
+                            GeometryReader{ geometry in
+                                TextEditor(
+                                    text: $text
+                                )
+                                .border(.red)
+                                .padding(.top, geometry.size.height * 0.1)
+                                .scrollContentBackground(.hidden)
+                                .multilineTextAlignment(.center)
+                                .frame(width: geometry.size.width * 0.7, height: geometry.size.height/2, alignment: .center)
+                                .frame(width: geometry.size.width, height: geometry.size.height)
+                            }
                     }
                 .padding()
+                .border(.pink)
                 Spacer()
-                StoreButtonView(isActive: isActiveQ4)
+                StoreButtonView(isActive: !text.isEmpty)
             }
         }
     }
 }
-
 
 struct QuestionView04_Previews: PreviewProvider {
     static var previews: some View {
