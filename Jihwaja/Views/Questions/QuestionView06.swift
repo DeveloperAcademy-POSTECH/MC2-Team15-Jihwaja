243b10deaf985df127fc8b8963e80a893971ef88
dimazsr <127843799+dimazsr@users.noreply.github.com>
2023-05-10 17:14:12 +0900

    ✨ feat: 추가 Question6 완성

--- Jihwaja/Views/Questions/QuestionView06.swift
+++ Jihwaja/Views/Questions/QuestionView06.swift
@@ -6,10 +6,13 @@
 //
 
 import SwiftUI
+import Foundation
 
 struct QuestionView06: View {
     @State var habit : String = ""
     @State var isActive : Bool = false
+    @State private var lines = [Line]()
+    @State private var deletedLines = [Line]()
     
     //처음 질문
     @State private var showModal = false
@@ -21,7 +24,10 @@ struct QuestionView06: View {
     var body: some View {
         ZStack {
             if eraseFlag {
-                EraseView()
+                VStack{
+                    QuestionView(question: "⚒️ 곽애숙씨의 고치고 싶은 습관이\n고쳐지길 바라면서 지워주세요")
+                    EraseView(initialHabit: habit)
+                }
             } else {
                     VStack {
                         ScrollView{
@@ -70,8 +76,13 @@ struct QuestionView06: View {
                             print("done")
                         }
                     }) {
-                        // habit.isEmpty == false 이면 isActive true
-                        StoreButtonView(isActive: !habit.isEmpty)
+                        if eraseFlag {
+                            // lines.isEmpty == false 이면 isActive true
+                            StoreButtonView(isActive: !lines.isEmpty)
+                        } else {
+                            // habit.isEmpty == false 이면 isActive true
+                            StoreButtonView(isActive: !habit.isEmpty)
+                        }
                     }
                     .sheet(isPresented: self.$showModal) {
                         HalfModalView(imageName: "test_gif", title: "힘껏 문지르기", content: "고치고 싶은 습관이 고쳐지길\n바라면서 힘껏 문질러주세요")
@@ -82,15 +93,46 @@ struct QuestionView06: View {
     }
 
 struct EraseView: View {
-    @State var food : String = ""
+    @State var habit : String = ""
     @State var isActive : Bool = false
     
+    @State private var lines = [Line]()
+    @State private var deletedLines = [Line]()
+    @State private var brushColor: Color = .white
+    @State private var brushWidth: CGFloat = 30
+    
+    let initialHabit: String
+    
     var body: some View {
+        
         VStack {
-            ScrollView{
-                // 질문 뷰
-                QuestionView(question: "⚒️ 곽애숙씨의 고치고 싶은 습관이\n고쳐지길 바라면서 지워주세요")
-                Spacer()
+            ScrollView {
+                
+                ZStack{
+                    Text("\(initialHabit)")
+                        .frame(width: getWidth() * 0.78, height: getHeight() * 0.5)
+                        .font(.system(size: 38))
+                        .fontWeight(.black)
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(Color("green"))
+                    
+                    // DrawingShape 호출
+                    ForEach(lines) { line in
+                        DrawingShape(points: line.points)
+                            .stroke(line.color, style: StrokeStyle(lineWidth: line.lineWidth, lineCap: .round, lineJoin: .round))
+                    }
+                }
+                .gesture(DragGesture(minimumDistance: 0, coordinateSpace: .local).onChanged({ value in
+                    let newPoint = value.location
+                    if value.translation.width + value.translation.height == 0 {
+                        lines.append(Line(points: [newPoint], color: brushColor, lineWidth: brushWidth))
+                    } else {
+                        let index = lines.count - 1
+                        lines[index].points.append(newPoint)
+                    }
+                }).onEnded({ value in
+                    if let lastLine = lines.last?.points, lastLine.isEmpty { lines.removeLast() }
+                }))
             }
         }
     }
