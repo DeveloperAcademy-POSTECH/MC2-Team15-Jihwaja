145b71474ba89015723f92fbfb480fc5966583e3
MK827 <bluebonnet712@gmail.com>
2023-05-10 12:46:16 +0900

    :sparkles: feat: 생성 Q5View (#16)

--- Jihwaja/Views/Questions/QuestionView05.swift
+++ Jihwaja/Views/Questions/QuestionView05.swift
@@ -8,13 +8,45 @@
 import SwiftUI
 
 struct QuestionView05: View {
+    @State private var circleSize: CGFloat = 0.4
+    @State private var isActiveQ5 = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack {
+            QuestionView(question: "😄 지금 곽애숙씨가 느끼고 있는\n행복의 크기를 알려주세요!")
+            Spacer()
+            ZStack{
+                Circle()
+                    .foregroundColor(Color("green"))
+                    .frame(width: getWidth() * 0.2 * circleSize)
+                Circle()
+                    .frame(width: getWidth()*0.8)
+                    .foregroundColor(Color("green"))
+                    .opacity(0.1)
+                    .gesture(
+                        MagnificationGesture()
+                            .onChanged{  CGFloatInGesture in
+                                circleSize =  min (CGFloatInGesture, 4.0)
+                                isActiveQ5 = true
+                            }
+                    )
+            }
+            Spacer()
+            Text("\(Int(circleSize * 25))%")
+                .font(.title)
+                .fontWeight(.bold)
+            Spacer()
+            Spacer()
+            Spacer()
+            Spacer()
+            StoreButtonView(isActive: isActiveQ5)
+        }
     }
 }
-
-struct QuestionView05_Previews: PreviewProvider {
-    static var previews: some View {
-        QuestionView05()
+    
+    struct QuestionView05_Previews: PreviewProvider {
+        static var previews: some View {
+            QuestionView05()
+        }
     }
-}
