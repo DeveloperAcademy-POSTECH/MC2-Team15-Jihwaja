9267d9f7963ed5010867957e3e2bd3cd5a45c40e
geee3 <c1as2icblu3@gmail.com>
2023-05-12 09:48:45 +0900

    📦️ feat: 추가 FLAnimatedImage (#32)

---
+++ Jihwaja/Managers/AnimatedManager.swift
@@ -0,0 +1,21 @@
+//
+//  AnimatedManager.swift
+//  Jihwaja
+//
+//  Created by geee3 on 2023/05/12.
+//
+
+import SwiftUI
+import FLAnimatedImage
+
+struct AnimatedManager: UIViewRepresentable {
+    let animatedView = FLAnimatedImageView()
+    var fileName: String
+    
+    func makeUIView(context: UIViewRepresentableContext<AnimatedManager>) -> UIView {
+        <#code#>
+    }
+    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<AnimatedManager>) {
+        <#code#>
+    }
+}
