88345b609223ed51722029d4a855ff0d6cbe5a55
geee3 <c1as2icblu3@gmail.com>
2023-05-12 10:44:35 +0900

    ✨ feat: 구현 AnimatedManager (#32)

--- Jihwaja/Managers/AnimatedManager.swift
+++ Jihwaja/Managers/AnimatedManager.swift
@@ -13,9 +13,24 @@ struct AnimatedManager: UIViewRepresentable {
     var fileName: String
     
     func makeUIView(context: UIViewRepresentableContext<AnimatedManager>) -> UIView {
-        <#code#>
+        let view = UIView()
+        
+        let path: String = Bundle.main.path(forResource: fileName, ofType: "gif")!
+        let url = URL(fileURLWithPath: path)
+        let gifData = try! Data(contentsOf: url)
+        
+        let gif = FLAnimatedImage(animatedGIFData: gifData)
+        animatedView.animatedImage = gif
+        
+        animatedView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(animatedView)
+        
+        NSLayoutConstraint.activate([
+            animatedView.heightAnchor.constraint(equalTo: view.heightAnchor),
+            animatedView.widthAnchor.constraint(equalTo: view.widthAnchor)])
+        
+        return view
     }
-    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<AnimatedManager>) {
-        <#code#>
-    }
+    
+    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<AnimatedManager>) {}
 }
