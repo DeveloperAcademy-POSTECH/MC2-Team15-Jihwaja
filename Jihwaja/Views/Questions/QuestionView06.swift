5ae3b231e582992269262ecc9454e942b73b5c79
juwonleee <juwonlee0509@gmail.com>
2023-05-14 23:15:52 +0900

    ✨feat: 추가 Q6View 모달 및 버튼 분기 및 화면 분기 기능 (#50)

--- Jihwaja/Views/Questions/QuestionView06.swift
+++ Jihwaja/Views/Questions/QuestionView06.swift
@@ -8,101 +8,97 @@
 import SwiftUI
 import Foundation
 
-struct QuestionView06: View {
+struct QuestionView06 : View {
+    
     @Environment(\.presentationMode) var presentationMode
     @EnvironmentObject var store: JihwajaStore
     
+    @State var isShowingEraseView = false
+    @State var showModal = true
+    @State var isActive = false
     
     @State var habit : String = ""
-    @State var isActive : Bool = false
     @State private var lines = [Line]()
     @State private var deletedLines = [Line]()
     
-    //처음 질문
-    @State private var showModal = false
-    //처음 질문에서 저장하기 눌렀을 때
-    @State private var secondFlag = false
-    //두번째 창
-    @State private var eraseFlag = false
-    
-    var body: some View {
-        ZStack {
-            if eraseFlag {
-                VStack{
-                    QuestionView(question: "⚒️ \(store.jihwaja.A1)씨의 고치고 싶은 습관이\n고쳐지길 바라면서 지워주세요")
-                    EraseView(lines: $lines, initialHabit: habit)
-                }
-            } else {
-                    VStack {
-                        ScrollView{
-                            // 질문 뷰
-                            QuestionView(question: "⚒️ \(store.jihwaja.A1)씨가 고치고 싶은\n습관을 적어주세요!")
-                            
-                            // 답변 TextEditor
-                            TextEditor(text: $habit)
-                                .padding(EdgeInsets(top: 0, leading: 5, bottom: 0, trailing: 0))
-                                .padding()
-                                .background(
-                                    RoundedRectangle(cornerRadius: 15)
-                                        .stroke(Color.gray, lineWidth: 1)
-                                )
-                                .cornerRadius(15)
-                                .frame(width: getWidth() * 0.78, height: getHeight() * 0.15)
-                            
-                            Spacer()
-                        }
-                    }
-                    .onAppear {
-                        // View가 로드될 때 키보드를 자동으로 띄워줌
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                            UIApplication.shared.sendAction(#selector(UIResponder.becomeFirstResponder), to: nil, from: nil, for: nil)
-                        }
-                    }
+    var body : some View {
+        if isShowingEraseView == true || store.jihwaja.isCompleted[5] == true {
+            VStack{
+                QuestionView(question: "⚒️ \(store.jihwaja.A1)씨의 고치고 싶은 습관이\n고쳐지길 바라면서 지워주세요")
+                EraseView(lines: $lines)
+                
+                Button(action:{
+                    store.jihwaja.isCompleted[5] = true
+                    self.presentationMode.wrappedValue.dismiss()
                     
-                    .onTapGesture {
-                        // 키보드 외 영역 터치 시 키보드를 내리도록 함
-                        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-                    }
+                }) {
+                    // lines.isEmpty == false 이면 isActive true
+                    StoreButtonView(isActive: !lines.isEmpty)
+                }.opacity(store.jihwaja.isCompleted[5] ? 0 : 1)
+            }
+            .sheet(isPresented: store.jihwaja.isCompleted[5] ? .constant(false) : $showModal)
+            { HalfModalView(imageName:"Q6_motion",
+                            title: "힘껏 문지르기",
+                            content: "고치고 싶은 습관이 고쳐지길\n바라면서 힘껏 문질러주세요",
+                            showModal: $showModal)
+            }
+            
+            
+        } else if isShowingEraseView == false {
+            VStack {
+                ScrollView{
+                    // 질문 뷰
+                    QuestionView(question: "⚒️ \(store.jihwaja.A1)씨가 고치고 싶은\n습관을 적어주세요!")
+                    
+                    // 답변 TextEditor
+                    TextEditor(text: $habit)
+                        .padding(EdgeInsets(top: 0, leading: 5, bottom: 0, trailing: 0))
+                        .padding()
+                        .background(
+                            RoundedRectangle(cornerRadius: 15)
+                                .stroke(Color.gray, lineWidth: 1)
+                        )
+                        .cornerRadius(15)
+                        .frame(width: getWidth() * 0.78, height: getHeight() * 0.15)
+                    
+                    Spacer()
                 }
                 
-                VStack {
-                    Spacer()
-                    // 저장 버튼
-                    Button(action:{
-                                            
-                        if !secondFlag {
-                            self.showModal = true
-                            self.secondFlag = true
-                            self.eraseFlag = true
-                        } else {
-                            // MainView 로 넘어가는 코드
-                            // 여섯 번째 답변 저장
-                            print("done")
-                            store.jihwaja.isCompleted[5] = true
-                            self.presentationMode.wrappedValue.dismiss()
-                        }
-                    }) {
-                            // lines.isEmpty == false 이면 isActive true
-                            StoreButtonView(isActive: eraseFlag ? !habit.isEmpty : !lines.isEmpty)
-                    }
-                    .sheet(isPresented: self.$showModal) {
-                        HalfModalView(imageName: "test_gif", title: "힘껏 문지르기", content: "고치고 싶은 습관이 고쳐지길\n바라면서 힘껏 문질러주세요", showModal: $showModal)
-                    }
+                // 저장 버튼
+                Button(action:{
+                    store.jihwaja.A6 = habit
+                    isShowingEraseView.toggle()
+                }) {
+                        // habit이 비어있지 않으면 isActive true
+                        StoreButtonView(isActive: !habit.isEmpty )
+                }
+            } // Vstack
+            .onAppear {
+                habit = store.jihwaja.A6
+                // View가 로드될 때 키보드를 자동으로 띄워줌
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                    UIApplication.shared.sendAction(#selector(UIResponder.becomeFirstResponder), to: nil, from: nil, for: nil)
                 }
             }
-        }
-    }
+            
+            .onTapGesture {
+                // 키보드 외 영역 터치 시 키보드를 내리도록 함
+                UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+            }
+        } // else
+    } // body
+} // View
+
 
 struct EraseView: View {
-    @State var habit : String = ""
+    
     @State var isActive : Bool = false
+    @EnvironmentObject var store: JihwajaStore
     
     @Binding var lines : [Line]
     @State private var deletedLines = [Line]()
     @State private var brushColor: Color = .white
     @State private var brushWidth: CGFloat = 30
-    
-    let initialHabit: String
     
     var body: some View {
         
@@ -110,7 +106,7 @@ struct EraseView: View {
             ScrollView {
                 
                 ZStack{
-                    Text("\(initialHabit)")
+                    Text("\(store.jihwaja.A6)")
                         .frame(width: getWidth() * 0.78, height: getHeight() * 0.5)
                         .font(.system(size: 38))
                         .fontWeight(.black)
@@ -134,6 +130,7 @@ struct EraseView: View {
                 }).onEnded({ value in
                     if let lastLine = lines.last?.points, lastLine.isEmpty { lines.removeLast() }
                 }))
+                .disabled(store.jihwaja.isCompleted[5])
             }
         }
     }
