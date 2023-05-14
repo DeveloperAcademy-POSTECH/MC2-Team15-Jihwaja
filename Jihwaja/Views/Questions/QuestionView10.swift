ad82d8d7ee5ce36dcda67559ba5d9d37ebb6789e
Henrykim1111 <henrykim9319@gmail.com>
2023-05-15 00:11:44 +0900

    ✨ feat: 생성 Q10View (#10)

--- Jihwaja/Views/Questions/QuestionView10.swift
+++ Jihwaja/Views/Questions/QuestionView10.swift
@@ -5,32 +5,7 @@
 //  Created by Minkyung Kim on 2023/05/02.
 //
 
-
-//struct QuestionView10: View {
-//
-//
-//
-
-//}
-//
-//struct QuestionView10_Previews: PreviewProvider {
-//    static var previews: some View {
-//        QuestionView10()
-//    }
-//}
-
-    
-
-//
-//  ContentView.swift
-//  0511_Home
-//
-//  Created by Hyungmin Kim on 2023/05/11.
-//
-
 import SwiftUI
-
-
 
 
 // edit it later
@@ -72,7 +47,10 @@ struct QuestionView10: View {
                         
                         if stage == 3 {
                             HStack() {
-                                CardView(imageName: selected[0])
+                                Button(action: {print("hi")})
+                                {
+                                    CardView(imageName: selected[0])
+                                }
                             }
                             .frame(width: IMAGE_WIDTHS + 50, height: IMAGE_HEIGHTS + 50)
                         }
@@ -98,15 +76,6 @@ struct QuestionView10: View {
                             }
                         }
                         
-                        
-                        
-                        //            .onChange(of: selected.count, perform: {
-                        //                // TODO: selected.count
-                        //                // When stage == 0 && count == 4, stage1: 2, stage2: 1
-                        //                // 2. stage + 1
-                        //                // 3. not_cards = selected, selected = []
-                        //                // 4. finish 처리
-                        //            })
                         
                         
                     }
@@ -161,97 +130,3 @@ struct Question10_Previews: PreviewProvider {
         QuestionView10()
     }
 }
-
-
-
-
-
-
-
-/*
-import SwiftUI
-
-struct Card {
-    let name: String
-    let image: String
-    var quarterFlag: Bool = false
-    var semiFlag: Bool = false
-    var finalFlag: Bool = false
-}
-
-var cards = [
-    Card(name: "Card 1", image: "cardDesign00"),
-    Card(name: "Card 2", image: "cardDesign01"),
-    Card(name: "Card 3", image: "cardDesign02"),
-    Card(name: "Card 4", image: "cardDesign03"),
-    Card(name: "Card 5", image: "cardDesign04"),
-    Card(name: "Card 6", image: "cardDesign05"),
-    Card(name: "Card 7", image: "cardDesign06"),
-    Card(name: "Card 8", image: "cardDesign07")
-]
-
-
-struct QuestionView10: View {
-    @State private var selectedCard: Int?
-    
-    var body: some View {
-        VStack {
-            HStack {
-                CardView(imageName: "cardDesign00")
-                    .opacity(selectedCard == 2 ? 0 : 1)
-                    .onTapGesture {
-                        // Select card 1
-                        selectedCard = 1
-                        cards[0].quarterFlag = true
-                        print(cards[0].quarterFlag)
-                    }
-                
-                CardView(imageName: "cardDesign01")
-                    .opacity(selectedCard == 1 ? 0 : 1)
-                    .onTapGesture {
-                        // Select card 2
-                        selectedCard = 2
-                    }
-            }
-            .animation(.default)
-            
-            Spacer()
-            
-            if selectedCard != nil {
-                CardView(imageName: "card\(selectedCard!)")
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .onTapGesture {
-                        selectedCard = nil
-                    }
-            }
-            
-            
-//            if let selectedCard = selectedCard {
-//                if selectedCard == 1 {
-//                    Text("Card 1 selected")
-//                } else {
-//                    Text("Card 2 selected")
-//                }
-//            }
-        }
-    }
-}
-
-struct CardView: View {
-    let imageName: String
-    
-    var body: some View {
-        Image(imageName)
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-            .frame(width: 200, height: 300)
-            .padding()
-    }
-}
-
-struct QuestionView10_Previews: PreviewProvider {
-    static var previews: some View {
-        QuestionView10()
-    }
-}
-*/
