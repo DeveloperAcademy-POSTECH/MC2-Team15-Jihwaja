b2220e4ac007f42ebab71d4b3805b7bc2175b197
dimazsr <127843799+dimazsr@users.noreply.github.com>
2023-05-15 02:00:36 +0900

    ✨ feat: 추가 Onboarding

---
+++ Jihwaja/Views/Onboarding/Onboarding1.swift
@@ -0,0 +1,74 @@
+//
+//  Onboarding1.swift
+//  Jihwaja
+//
+//  Created by 안수린 on 2023/05/15.
+//
+
+import SwiftUI
+
+struct Onboarding1: View {
+    var body: some View {
+        VStack {
+            
+            Text("知 化 自")
+                .font(.system(size: 32))
+                .fontWeight(.heavy)
+                .foregroundColor(Color("grayText"))
+                .padding(.top, getWidth() * 0.18)
+                .padding(.bottom, getWidth() * 0.001)
+            Text("알 지 / 될 화 / 스스로 자")
+                .font(.system(size: 21))
+                .fontWeight(.semibold)
+                .foregroundColor(Color("grayText"))
+            
+            Spacer()
+            Image("SplashImg")
+                .resizable()
+                .frame(width: getWidth() * 0.2, height: getHeight() * 0.2)
+            Spacer()
+            
+            HStack {
+                Text("스스로를 돌아볼 시간")
+                    .font(.system(size: 21))
+                    .fontWeight(.heavy)
+                    .padding(.trailing, -8)
+                Text("이 많아진")
+                    .font(.system(size: 21))
+                    .fontWeight(.semibold)
+            }
+            
+            Text("부모님들을 위한 앱입니다.")
+                .font(.system(size: 21))
+                .fontWeight(.semibold)
+                .padding(.bottom, getWidth() * 0.05)
+            
+            Text("질문에 답을 하는 과정을 통해")
+                .font(.system(size: 21))
+                .fontWeight(.semibold)
+            
+            HStack {
+                Text("나 자신에 대해 더 깊게 이해")
+                    .font(.system(size: 21))
+                    .fontWeight(.heavy)
+                    .padding(.trailing, -8)
+                Text("하고")
+                    .font(.system(size: 21))
+                    .fontWeight(.semibold)
+            }
+            
+            Text("행복을 찾아보세요!")
+                .font(.system(size: 21))
+                .fontWeight(.semibold)
+                .padding(.bottom, getWidth() * 0.07)
+            
+        }
+        .multilineTextAlignment(.center)
+    }
+}
+
+struct Onboarding1_Previews: PreviewProvider {
+    static var previews: some View {
+        Onboarding1()
+    }
+}
---
+++ Jihwaja/Views/Onboarding/Onboarding4.swift
@@ -0,0 +1,45 @@
+//
+//  Onboarding4.swift
+//  Jihwaja
+//
+//  Created by 안수린 on 2023/05/15.
+//
+
+import SwiftUI
+
+struct Onboarding4: View {
+    var body: some View {
+        VStack {
+            
+            Text("모든 질문에 답을 완료하면")
+            .font(.system(size: 21))
+            .fontWeight(.semibold)
+            .padding(.top, getWidth() * 0.18)
+            
+            HStack {
+                Text("최종 결과지가 생성")
+                    .font(.system(size: 21))
+                    .fontWeight(.heavy)
+                    .padding(.trailing, -8)
+                Text("됩니다!")
+                .font(.system(size: 21))
+                .fontWeight(.semibold)
+                      }
+            
+            Spacer()
+            
+            Image("iPhoneLoad")
+                .resizable()
+                .frame(width: 200, height: 400)
+                .padding(.bottom, getWidth() * 0.03)
+            
+        }
+        .multilineTextAlignment(.center)
+    }
+}
+
+struct Onboarding4_Previews: PreviewProvider {
+    static var previews: some View {
+        Onboarding4()
+    }
+}
